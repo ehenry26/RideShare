6a55887c9ce8cdfb10dc28698d244d9bcd310dbb
Elissa Henry <ehenry26@students.polytechnic.org>
2025-02-24 09:09:54 -0800

    Made tester and edited person

---
+++ RideShareTester.java
@@ -0,0 +1,10 @@
+public class RideShareTester {
+    public static void main(String[] args) {
+        Person a = new Person(5,7);
+        Person b = new Person(2,0);
+        Person c = new Person(10, 24);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
