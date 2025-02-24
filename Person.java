eff64b54e511004158557940f541c0ee477cd231
Elissa Henry <ehenry26@students.polytechnic.org>
2025-02-24 09:07:34 -0800

    Made person

---
+++ Person.java
@@ -0,0 +1,31 @@
+public class Person{
+    //fields
+    private int destination;
+    private boolean direction; //true for Right, false for Left
+    private int startLocation;
+
+    //constructor
+    public Person(int myDestination, int myStart){
+        destination = myDestination;
+        startLocation = myStart;
+        if(destination > startLocation){
+            direction = true;
+        } else {
+            direction = false;
+        }
+    }
+
+    //methods
+    public int getDestination(){
+        return destination;
+    }
+    public boolean getDirection(){
+        return direction;
+    }
+    public int getStart(){
+        return startLocation;
+    }
+    public String toString(){
+        return super.toString() + "Start: " + startLocation + ", Destination: " + destination + ", Going right? " + direction;
+    }
+}
