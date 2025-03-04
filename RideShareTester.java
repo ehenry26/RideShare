38ca91f2af2a7534f5de096541428f3fa00dc9c2
Elissa Henry <ehenry26@students.polytechnic.org>
2025-03-04 09:09:03 -0800

    Tester edits

--- RideShareTester.java
+++ RideShareTester.java
@@ -1,15 +1,10 @@
 public class RideShareTester {
     public static void main(String[] args) {
-        Person a = new Person(5,7);
-        Person b = new Person(2,0);
-        Person c = new Person(10, 24);
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        Car n = new Car(5, 9);
-        System.out.println(n);
-        n.addPassenger(a);
-        System.out.println(n);
-        System.out.println(n.unload());
+        Road r = new Road();
+        System.out.println(r.toString());
+        r.fillCars(5);
+        r.fillStations(10);
+        System.out.println("-----------");
+        
     }
 }
