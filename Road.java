1ba11f5a845a2452e20bc7525e74ee405aeff291
Elissa Henry <ehenry26@students.polytechnic.org>
2025-03-04 09:26:03 -0800

    removed old move method and added readme text file

--- Road.java
+++ Road.java
@@ -19,26 +19,6 @@ public class Road {
     }
 
     //methods
-    /**
-     * Method for moving all cars- left and right- if they meet requirements
-     * I received help from Kelly on this part
-     * 02/28/2025
-     */
-   // public void moveAll(){
-        //for(Car car : cars){
-            //while(car.hasRoom()){
-                //if(car.getCurrentLocation() > car.getDestination() && stations.get(car.getCurrentLocation()).getnextL() != null){
-                    //car.addPassenger(stations.get(car.getCurrentLocation()).getnextL());
-
-                //} else if (car.getCurrentLocation() > car.getDestination() && stations.get(car.getCurrentLocation()).getnextR() != null){
-                    //car.addPassenger(stations.get((car.getCurrentLocation()).getnextR());
-                //} else{
-                    //break;
-                //}
-            //}
-        //}
-        //}
-
     //POPULATE STATIONS
     public void fillStations(int numPeople){
         for(int i = 0; i < numPeople; i++){
